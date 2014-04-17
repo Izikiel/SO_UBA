d5e2ee0d5c8533ad78b5d383fb226364e0b145ac
Izikiel <izikielrules@gmail.com>
2014-04-17 15:47:48 -0300

    Tasks.cpp by martin

--- tp1/codigo/tasks.cpp
+++ tp1/codigo/tasks.cpp
@@ -1,7 +1,10 @@
 #include "tasks.h"
 #include <random>
 #include <chrono>
-
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <iostream>
 using namespace std;
 
 void TaskCPU(int pid, vector<int> params) { // params: n
@@ -30,6 +33,35 @@ void TaskConsola(int pid, vector<int> params) { // params: n, bmin, bmax, ...
 	}
 }
 
+void TaskBatch(int pid, vector<int> params) { // params: total_cpu, cant_bloqueos
+	int total_cpu = params[0], cant_bloqueos = params[1], i = 0;
+	unsigned seed1 = std::chrono::system_clock::now().time_since_epoch().count();
+	default_random_engine generator(seed1);
+	uniform_int_distribution<int> distribution(0, total_cpu-1);
+	vector<int> moments;
+	set<int> moments_used;
+	while(i < cant_bloqueos) {
+		int m = distribution(generator);
+		if (moments_used.find(m) == moments_used.end()) {
+			moments_used.insert(m);
+			moments.push_back(m);
+			i++;
+		}
+	}
+	sort(moments.begin(), moments.end());
+	int act_moment = 0;
+	for (i = 0; i < cant_bloqueos; i++) {
+		cout << moments[i] << endl;
+		if (act_moment != moments[i]) {
+			uso_CPU(pid, moments[i]-act_moment);
+		}
+		uso_IO(pid, 1);
+		act_moment = moments[i] + 1;
+	}
+	if (act_moment < total_cpu-1) {
+		uso_CPU(pid, total_cpu-1 - act_moment);
+	}
+}
 
 void tasks_init(void) {
 	/* Todos los tipos de tareas se deben registrar acá para poder ser usadas.
@@ -39,4 +71,5 @@ void tasks_init(void) {
 	register_task(TaskIO, 2);
 	register_task(TaskAlterno, -1);
 	register_task(TaskConsola, 3);
+	register_task(TaskBatch, 2);
 }
