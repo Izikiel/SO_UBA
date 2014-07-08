7da418c21d05eac6bb2ddf0002a754077cdb128a
Ezequiel Dario Gambaccini <ezequiel.gambaccini@gmail.com>
2014-07-08 15:47:05 -0300

    agrego para matar daemon de mongo

--- Tp3/data/get_data.sh
+++ Tp3/data/get_data.sh
@@ -5,4 +5,6 @@ curl http://so.exp.dc.uba.ar/vm/TP3.tar.gz > TP3.tar.gz
 echo "uncompressing"
 tar -xzvf TP3.tar.gz
 echo "setting for db daemon"
+rm -f mongod.lock
+kill -15 $(pgrep -f mongod)
 mongod --dbpath .
